6e95b2e05b7a0335439bde78ab03a4561d89eb69
Денис <denis-garifullin.ya@ya.ru>
2022-01-11 16:34:37 +0500

    add start

---
+++ config.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "1342191406:AAG9kauldTvxJ0nHy5cwoLVeO9Cr-byPLzw"
+APP_URL = "https://denis-garifullin-assist.herokuapp.com/" + BOT_TOKEN
+DB_URI = ""
---
+++ main.py
@@ -0,0 +1,71 @@
+import os
+import telebot
+import logging
+import psycopg2
+from config import *
+from flask import Flask, request
+
+bot = telebot.TeleBot(BOT_TOKEN)
+server = Flask(__name__)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
+
+db_connection = psycopg2.connect(DB_URI, sslmode="require")
+db_object = db_connection.cursor()
+
+
+def update_messages_count(user_id):
+    db_object.execute(f"UPDATE users SET messages = messages + 1 WHERE id = {user_id}")
+    db_connection.commit()
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    user_id = message.from_user.id
+    username = message.from_user.username
+    bot.reply_to(message, f"Hello, {username}!")
+
+"""    db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
+    result = db_object.fetchone()
+
+    if not result:
+        db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
+        db_connection.commit()
+
+    update_messages_count(user_id)
+
+
+@bot.message_handler(commands=["stats"])
+def get_stats(message):
+    db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT 10")
+    result = db_object.fetchall()
+
+    if not result:
+        bot.reply_to(message, "No data...")
+    else:
+        reply_message = "- Top flooders:\n"
+        for i, item in enumerate(result):
+            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
+        bot.reply_to(message, reply_message)
+
+    update_messages_count(message.from_user.id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def message_from_user(message):
+    user_id = message.from_user.id
+    update_messages_count(user_id) """
+
+
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
