f4f1707dfde1d34c6930193478654a46505b2e00
Денис <denis-garifullin.ya@ya.ru>
2022-01-12 16:40:31 +0500

    delet query db

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ db_connection = psycopg2.connect(DB_URI, sslmode="require")
 db_object = db_connection.cursor()
 
 
-def update_messages_count(user_id):
+"""def update_messages_count(user_id):
     db_object.execute(f"UPDATE users SET messages = messages + 1 WHERE id = {user_id}")
-    db_connection.commit()
+    db_connection.commit()"""
 
 
 @bot.message_handler(commands=["start"])
